a135ced4958fe903468757865c808af30a872813
WaiZack <syz1992@gmail.com>
2014-06-27 00:22:46 +0800

    added new pages
    added the post login pages and got a new nav bar

--- idea.py
+++ idea.py
@@ -13,32 +13,58 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     """ Handler for the front page."""
-
     def get(self):
         template = jinja_environment.get_template('index2.html')
         self.response.out.write(template.render())
 
 class About(webapp2.RequestHandler):
+    """ Handler for the about page."""
     def get(self):
         template = jinja_environment.get_template('about.html')
         self.response.out.write(template.render())
 
+class LoggedMain(webapp2.RequestHandler):
+    """ Handler for the post login page"""
+    def get(self):
+        template_values = {
+                'user_mail': users.get_current_user().email(),
+                'logout': users.create_logout_url(self.request.host_url),
+            }
+        template  = jinja_environment.get_template('logged.html')
+        self.response.out.write(template.render(template_values))
+
+class Add(webapp2.RequestHandler):
+    def get (self):
+        template_values = {
+                'user_mail': users.get_current_user().email(),
+                'logout': users.create_logout_url(self.request.host_url),
+            }
+        template  = jinja_environment.get_template('add.html')
+        self.response.out.write(template.render(template_values))
+
+
+
+
 class GoogleOpenId(webapp2.RequestHandler):
+    """ Handler for Google Login."""
     def post(self):
         openId = "https://www.google.com/accounts/o8/id"
-        self.redirect(users.create_login_url('/', None, federated_identity=openId))
+        self.redirect(users.create_login_url('/logged/main', None, federated_identity=openId))
 
 class NUSOpenId(webapp2.RequestHandler):
+    """ Handler for NUS Login."""
     def post(self):
         openId = "https://openid.nus.edu.sg/"
-        self.redirect(users.create_login_url('/', None, federated_identity=openId))
+        self.redirect(users.create_login_url('/logged/main', None, federated_identity=openId))
 
 class YahooOpenId(webapp2.RequestHandler):
+    """ Handler for Yahoo Login."""
     def post(self):
         openId = "yahoo.com"
-        self.redirect(users.create_login_url('/', None, federated_identity=openId))
+        self.redirect(users.create_login_url('/logged/main', None, federated_identity=openId))
 
 class HandleOpenId(webapp2.RequestHandler):
+    """ Handler for pages that require login."""
     def get(self):
         self.redirect(self.request.host_url)
 
@@ -47,6 +73,8 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/NUSOpenId', NUSOpenId),
                                ('/YahooOpenId', YahooOpenId),
                                ('/_ah/login_required', HandleOpenId),
+                               ('/logged/main',LoggedMain),
+                               ('/logged/add',Add),
                                ('/about',About),
                               ],
                               debug=True)
