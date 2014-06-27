ce055f9209313a790ce4fe2284374c74dffd2c75
WaiZack <syz1992@gmail.com>
2014-06-27 22:38:01 +0800

    added the adding option
    added the add page and browse to be added at a later time

--- idea.py
+++ idea.py
@@ -26,24 +26,28 @@ class About(webapp2.RequestHandler):
 class LoggedMain(webapp2.RequestHandler):
     """ Handler for the post login page"""
     def get(self):
-        template_values = {
+            template_values = {
                 'user_mail': users.get_current_user().email(),
                 'logout': users.create_logout_url(self.request.host_url),
             }
-        template  = jinja_environment.get_template('logged.html')
-        self.response.out.write(template.render(template_values))
+            template = jinja_environment.get_template('logged.html')
+            self.response.out.write(template.render(template_values))
 
 class Add(webapp2.RequestHandler):
-    def get (self):
-        template_values = {
+    def get(self):
+            template_values = {
                 'user_mail': users.get_current_user().email(),
                 'logout': users.create_logout_url(self.request.host_url),
             }
-        template  = jinja_environment.get_template('add.html')
-        self.response.out.write(template.render(template_values))
+            template = jinja_environment.get_template('add.html')
+            self.response.out.write(template.render(template_values))
 
-
-
+    def post (self):
+        idea = Idea(id = users.get_current_user().email())
+        idea.author = users.get_current_user().email()
+        idea.name = self.request.get("name")
+        idea.description = self.request.get("description")
+        idea.requirements = self.request.get("requirements")
 
 class GoogleOpenId(webapp2.RequestHandler):
     """ Handler for Google Login."""
@@ -68,13 +72,22 @@ class HandleOpenId(webapp2.RequestHandler):
     def get(self):
         self.redirect(self.request.host_url)
 
+class Idea(ndb.Model):
+    # Models an idea with name description requirements and author. Key is author
+    author = ndb.StringProperty()
+    name = ndb.StringProperty()
+    description = ndb.TextProperty()
+    requirements = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/GoogleOpenId', GoogleOpenId),
                                ('/NUSOpenId', NUSOpenId),
                                ('/YahooOpenId', YahooOpenId),
                                ('/_ah/login_required', HandleOpenId),
-                               ('/logged/main',LoggedMain),
-                               ('/logged/add',Add),
-                               ('/about',About),
+                               ('/logged/main', LoggedMain),
+                               ('/logged/add', Add),
+                               ('/about', About),
                               ],
                               debug=True)
