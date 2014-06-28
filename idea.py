e23d5cfa3fee34c229199e732bd025e9f73e41bf
WaiZack <syz1992@gmail.com>
2014-06-28 20:42:13 +0800

    Added query and browse function
    Added query and browse function

--- idea.py
+++ idea.py
@@ -10,6 +10,14 @@ from google.appengine.api import users
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/pages"))
 
+class Idea(ndb.Model):
+    # Models an idea with name description requirements and author. Key is author
+    author = ndb.StringProperty()
+    name = ndb.StringProperty()
+    description = ndb.TextProperty()
+    requirements = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class MainPage(webapp2.RequestHandler):
     """ Handler for the front page."""
@@ -34,7 +42,7 @@ class LoggedMain(webapp2.RequestHandler):
             self.response.out.write(template.render(template_values))
 
 class Add(webapp2.RequestHandler):
-    def get(self):
+    def show(self):
             template_values = {
                 'user_mail': users.get_current_user().email(),
                 'logout': users.create_logout_url(self.request.host_url),
@@ -42,12 +50,40 @@ class Add(webapp2.RequestHandler):
             template = jinja_environment.get_template('add.html')
             self.response.out.write(template.render(template_values))
 
-    def post (self):
-        idea = Idea(id = users.get_current_user().email())
+    def get(self):
+        self.show()
+
+    def post(self):
+        parent = ndb.Key('Ideastore','default_name')
+
+        idea = Idea(parent=parent)
         idea.author = users.get_current_user().email()
         idea.name = self.request.get("name")
         idea.description = self.request.get("description")
         idea.requirements = self.request.get("requirements")
+        idea.put()
+        self.show()
+
+class Browse(webapp2.RequestHandler):
+
+    def get(self):
+        self.show()
+
+    def show(self):
+        parent_key = ndb.Key('Ideastore','default_name')
+
+        idea_query = Idea.query()
+        idea_query.order(-Idea.name, Idea.date)
+        idea = idea_query.fetch()
+        template_values = {
+            'user_mail': users.get_current_user().email(),
+            'logout': users.create_logout_url(self.request.host_url),
+            'ideas': idea
+        }
+        template = jinja_environment.get_template('browse.html')
+        self.response.out.write(template.render(template_values))
+
+
 
 class GoogleOpenId(webapp2.RequestHandler):
     """ Handler for Google Login."""
@@ -72,13 +108,6 @@ class HandleOpenId(webapp2.RequestHandler):
     def get(self):
         self.redirect(self.request.host_url)
 
-class Idea(ndb.Model):
-    # Models an idea with name description requirements and author. Key is author
-    author = ndb.StringProperty()
-    name = ndb.StringProperty()
-    description = ndb.TextProperty()
-    requirements = ndb.TextProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
@@ -88,6 +117,7 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/_ah/login_required', HandleOpenId),
                                ('/logged/main', LoggedMain),
                                ('/logged/add', Add),
+                               ('/logged/browse', Browse),
                                ('/about', About),
                               ],
                               debug=True)
